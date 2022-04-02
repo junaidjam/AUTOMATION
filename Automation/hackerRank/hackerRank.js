799180e1ad1d91fc5bd63fdc588f05913fff89b1
junaidjam <junaidakhtarjam@gmail.com>
2022-04-03 01:55:14 +0530

    login successfully

--- Automation/hackerRank/hackerRank.js
+++ Automation/hackerRank/hackerRank.js
@@ -1,5 +1,8 @@
 const puppeteer = require("puppeteer");
 
+let email = "junaidakhtarjam@gmail.com";
+let password = "Junaid@123";
+let cTab;
 let browserOPenPromise = puppeteer.launch({
     headless: false,
     defaultViewportnull: null,
@@ -15,7 +18,7 @@ browserOPenPromise
         return allTabsPromise;
     })
     .then(function(allTabs){
-        let cTab = allTabs[0];
+        cTab = allTabs[0];
         console.log("new Tab");
         //URL to navigate page to
         let visitingLoginPagePromise = cTab.goto("https://www.hackerrank.com/auth/login");
@@ -23,6 +26,21 @@ browserOPenPromise
     })
     .then(function(){
         console.log("opened hackerrank login page");
+        let emailWillBeTypedPromise = cTab.type("input[name ='username']",email);
+        return emailWillBeTypedPromise;
+    })
+    .then(function(){
+        console.log("email typed");
+        let passwordWillBeTypedPromise = cTab.type("input[type = 'password']",password);
+        return passwordWillBeTypedPromise;
+    })
+    .then(function(){
+        console.log("password is typed");
+        let willBeLoggedInPromise = cTab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+        return willBeLoggedInPromise;
+    })
+    .then(function(){
+        console.log("Successfully Logged In");
     })
     .catch(function(err){
         console.log(err);
