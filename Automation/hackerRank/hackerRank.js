85b5ef8280ae7a269ef0e5033652567024341284
junaidjam <junaidakhtarjam@gmail.com>
2022-04-03 02:33:04 +0530

    login process

--- Automation/hackerRank/hackerRank.js
+++ Automation/hackerRank/hackerRank.js
@@ -1,8 +1,9 @@
 const puppeteer = require("puppeteer");
 
-let email = "junaidakhtarjam@gmail.com";
-let password = "Junaid@123";
-let cTab;
+// let {email} = 
+let email = "";
+let password = "";
+let curTab;
 let browserOPenPromise = puppeteer.launch({
     headless: false,
     defaultViewportnull: null,
@@ -18,25 +19,25 @@ browserOPenPromise
         return allTabsPromise;
     })
     .then(function(allTabs){
-        cTab = allTabs[0];
+        curTab = allTabs[0];
         console.log("new Tab");
         //URL to navigate page to
-        let visitingLoginPagePromise = cTab.goto("https://www.hackerrank.com/auth/login");
+        let visitingLoginPagePromise = curTab.goto("https://www.hackerrank.com/auth/login");
         return visitingLoginPagePromise;
     })
     .then(function(){
         console.log("opened hackerrank login page");
-        let emailWillBeTypedPromise = cTab.type("input[name ='username']",email);
+        let emailWillBeTypedPromise = curTab.type("input[name ='username']",email);
         return emailWillBeTypedPromise;
     })
     .then(function(){
         console.log("email typed");
-        let passwordWillBeTypedPromise = cTab.type("input[type = 'password']",password);
+        let passwordWillBeTypedPromise = curTab.type("input[type = 'password']",password);
         return passwordWillBeTypedPromise;
     })
     .then(function(){
         console.log("password is typed");
-        let willBeLoggedInPromise = cTab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+        let willBeLoggedInPromise = curTab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
         return willBeLoggedInPromise;
     })
     .then(function(){
